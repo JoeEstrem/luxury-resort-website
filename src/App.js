865d211c56f0d2dfc09594c00497a7a3ff833a8d
Joe Estrem <estremjoe@gmail.com>
2024-11-22 16:06:25 +0900

    made landing area

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar';
+import Landing from './components/Landing';
 
 
 
 function App() {
   return (
-    <NavBar />
+    <div>
+      <NavBar />
+      <Landing />
+    </div>
   );
 }
 
