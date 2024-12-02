258c838d5745b15aa5d3465692b50c82c6b4d4fa
Joe Estrem <estremjoe@gmail.com>
2024-12-02 14:27:15 +0900

    added design elements

---
+++ src/components/Spacer.js
@@ -0,0 +1,14 @@
+import "./Spacer.css"
+
+import React from 'react';
+
+function Spacer() {
+  return (
+    <div>
+      <div className="spacer-container-top"></div>
+      <div className="spacer-container-bottom"></div>
+    </div>
+  )
+}
+
+export default Spacer;
